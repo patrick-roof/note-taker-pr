647652afc48644a9961ceebe9e0adf786e8d6e31
patrick <roosterruf@gmail.com>
2023-01-17 19:31:35 -0700

    added code to server.js, uuid, edited db.json

--- server.js
+++ server.js
@@ -1,27 +1,68 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const util = require('util');
+const uuid = require('./Develop/helpers/uuid')
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('./Develop/public'));
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-
-//app.get function, may need to change
-app.get('/api/notes', (req, res) =>
-    readFileAsync('./Develop/db/db.json', 'utf8').then(function(data) {
-        notes = [].concat(JSON.parse(data))
-        res.json(notes);
-    })
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'))
 )
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'))
+)
+
+app.get('/api/notes', (req, res) => {
+
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            parsedNotes = JSON.parse(data)
+            res.json(parsedNotes)
+        }
+    })
+
+});
+
+app.post('/api/notes', (req, res) => {
+
+    const { title, text } = req.body;
+
+    const newNote = {
+        title,
+        text,
+        review_id: uuid(),
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+
+            parsedNotes.push(newNote);
+
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(parsedNotes, null),
+                (writeErr) =>
+                    writeErr
+                        ? console.error(writeErr)
+                        : console.log('Successfully updated notes!')
+            )
+        }
+    }) 
+})
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
